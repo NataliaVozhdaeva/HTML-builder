24f0f443410481f7ebf68228d8d36fd55ac34452
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-05-07 21:35:28 +0300

    feat: add solving 02-wrrite-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
+const process = require('node:process');
 const { stdin, stdout } = process;
-//const  { stdin as input, stdout as output } = require('node:process');
 const path = require('path');
 const readline = require('node:readline/promises');
 
@@ -8,14 +8,30 @@ const filepath = path.join(__dirname, 'text.txt');
 const input = stdin;
 const output = stdout;
 const fsp = require('fs/promises');
+const bayMsg = "sorry you're leaving so early \n";
+const rl = readline.createInterface({ input, output });
+const question = 'Write anything \n';
 
 fs.writeFile(filepath, '', (err) => {
   if (err) throw err;
 });
 
+output.write(question);
+
 async function foo() {
-  const rl = readline.createInterface({ input, output });
-  const answer = await rl.question('Write anything \n');
-  await fsp.appendFile(filepath, answer);
+  const answer = await rl.question('');
+
+  process.on('exit', () => {
+    stdout.write(bayMsg);
+  });
+
+  if (answer === 'exit') {
+    output.write(bayMsg);
+    process.exit();
+  }
+
+  fsp.appendFile(filepath, answer + '\n');
+  foo();
 }
+
 foo();
