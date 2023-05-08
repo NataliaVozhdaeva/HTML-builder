548f995e603ebf38ac64da7f104d6e079ccf6b59
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-05-08 14:24:16 +0300

    feat: add solving 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,11 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const dirName = path.join(__dirname, 'secret-folder');
 
-fs.readdir(dirName, function (err, files) {
+const getInfo = function (file) {
+  if (file.isFile()) {
+    let data = [];
+    fs.stat(path.resolve(__dirname, 'secret-folder', file.name), function (err, stats) {
+      if (err) {
+        return console.log(err);
+      }
+      data.push(file.name.split('.').slice(0, -1));
+      data.push(path.extname(file.name).slice(1));
+      data.push(stats.size + 'b');
+      console.log(data.join('-'));
+    });
+  }
+};
+
+fs.readdir(path.join(dirName), { withFileTypes: true }, function (err, files) {
   if (err) {
     return console.log(err);
   }
   files.forEach((item) => {
-    console.log(item);
+    getInfo(item);
   });
 });
