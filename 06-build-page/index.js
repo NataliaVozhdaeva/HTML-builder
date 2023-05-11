d2b10d2a0105808f2e91b04cefad2ca11fca8cfb
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-05-11 16:54:17 +0300

    feat: add CSS-builder to task 6: WIP

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,21 +1,19 @@
 const { promises: fs, constants } = require('fs');
 const path = require('path');
 const fsPromises = require('fs').promises;
-const filepathHTML = path.join(__dirname, 'project-dist', 'index.hlml');
+const filepathHTML = path.join(__dirname, 'project-dist', 'index.html');
 const destDir = path.join(__dirname, 'project-dist');
 const templateHTML = path.join(__dirname, 'template.html');
 const componentsDir = path.join(__dirname, 'components');
+const stylesDir = path.join(__dirname, 'styles');
 
 const buildHTML = async function (source, target) {
   await fs.copyFile(source, target);
-
-  //const data = await fs.readFile(filepathHTML);
 
   let data = await fsPromises.readFile(filepathHTML);
   const re = new RegExp(/{{[a-z]+}}/g);
 
   const componentsList = data.toString().match(re);
-  //console.log(componentsList);
 
   for (const el of componentsList) {
     const elName = el.slice(2, -2);
@@ -23,9 +21,20 @@ const buildHTML = async function (source, target) {
     data = data.toString().replace(el, elData);
   }
 
-  /*  const result = data.toString().replace(re, 'aaa');*/
-
   fs.writeFile(filepathHTML, data);
+};
+
+const buildCSS = async function (dir) {
+  let files = await fsPromises.readdir(dir, { withFileTypes: true });
+
+  files.forEach(async function (item) {
+    //console.log(item);
+
+    const elData = await fsPromises.readFile(path.join(__dirname, 'styles', item.name));
+    // console.log(elData.toString());
+
+    fsPromises.appendFile(path.join(destDir, 'style.css'), elData.toString());
+  });
 };
 
 const buildPage = async function (dest) {
@@ -41,6 +50,7 @@ const buildPage = async function (dest) {
   await fs.mkdir(destDir, { recursive: true });
 
   buildHTML(templateHTML, filepathHTML);
+  buildCSS(stylesDir);
 };
 
 buildPage(destDir);
